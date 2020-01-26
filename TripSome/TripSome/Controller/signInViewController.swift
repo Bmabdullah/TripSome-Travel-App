315c1e5dc95ef6407e7a4ec0da671efc11bf9d9d
AL Mustakim <almustakim@ALs-MacBook-Pro.local>
2020-01-26 21:36:38 +0600

    aded sign in functionality and changed some design

--- TripSome/TripSome/Controller/signInViewController.swift
+++ TripSome/TripSome/Controller/signInViewController.swift
@@ -14,16 +14,49 @@ class signInViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        buttonWork()
     }
     
 
     @IBOutlet weak var loginEmailTextField: UITextField!
     @IBOutlet weak var loginPassTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    
+    
+    let email = "bma@gmail.com"
+    let pass = "1234"
     
 
     @IBAction func signInButton(_ sender: Any) {
-        
-        print("SIgned In Succesfully")
+        if loginEmailTextField.text == email && loginPassTextField.text == pass
+        {
+            performSegue(withIdentifier: "signintohome", sender: self)
+                  print("SIgned In Succesfully")
+        }
+        else{
+            var message = ""
+            if loginEmailTextField.text == email && loginPassTextField.text != pass{
+                message = " please check your password "
+            }
+            else if loginEmailTextField.text != email && loginPassTextField.text == pass{
+                
+                message = " please check your email "
+            }
+            else{
+                message = " please check your email and Password "
+            }
+            
+            
+            let alert = UIAlertController(title: "Wrong Inormation", message: "\(message)", preferredStyle: .alert)
+            //alert.addAction(title="click",sty)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            //self.(alert, animated: true, completion: nil)
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+      
     }
     
     @IBAction func signUpPageButton(_ sender: Any) {
@@ -31,5 +64,12 @@ class signInViewController: UIViewController {
         //vc.
         performSegue(withIdentifier: "signintosignup", sender: self)
     }
+    
+    func buttonWork(){
+        signInButton.layer.borderColor = UIColor.green.cgColor
+        signInButton.layer.borderWidth = 2
+        signInButton.layer.cornerRadius = 20
+    }
+    
     
 }
